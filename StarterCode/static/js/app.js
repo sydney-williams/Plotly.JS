444b2b18bd202fb7c231bbcd923f228cab6a0855
sydney-williams <70979348+sydney-williams@users.noreply.github.com>
2021-02-25 23:13:09 -0600

    updated js

---
+++ StarterCode/static/js/app.js
@@ -0,0 +1,52 @@
+//read in sample.json file
+//use d3.json to read in local file
+
+d3.json("samples.json").then(function(data) {
+    console.log(data);
+    console.log(data['names']);
+    //metadata = array object
+    //metadata is an array object that holds subjects information (id,gender,age,etc.)
+    //use metadata to read the demographic info
+    var metadata = data['metadata'];
+    console.log(metadata);
+    //filter the metadata in order to retrieve a single id in the 153 items in the metadata array 
+
+    // create an empty array to add 'names' to 
+    // var names_from_data_names = data['names'];
+    // console.log(names_from_data_names);
+    // for (var i = 0; i < data['names'].length; i++){
+    //   dropDown.push(data['names'].length)
+    // }
+
+    var names_from_data_names = data['names'];    
+    var sel = document.getElementById('selDataset');
+    for(var i = 0; i < data['names'].length; i++) {
+    var opt = document.createElement('option');
+    opt.innerHTML = names_from_data_names[i];
+    opt.value = names_from_data_names[i];
+    sel.appendChild(opt);
+    }
+});
+  
+// function optionChanged() {
+//     console.log(new_value)
+//     new_value = 
+//     barPlot(new_value)
+// }
+
+// function barPlot(new_value) {
+//     PLotly the new id
+// }
+
+
+d3.json("samples.json").then(function metadata(id){
+  console.log(id);
+  metadata.filter(sampleObj => sampleObj.id == sample);
+}
+
+function optionChanged(id){
+  metadata(id);
+
+
+}
+
