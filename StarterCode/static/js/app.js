9583b57a1e573fb542202f92a804638c8a8fbfa0
sydney-williams <70979348+sydney-williams@users.noreply.github.com>
2021-02-26 22:50:55 -0600

    updated dropdown menu. now able to select test no id and see demographics.

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -2,13 +2,13 @@
 //use d3.json to read in local file
 
 d3.json("samples.json").then(function(data) {
-    console.log(data);
-    console.log(data['names']);
+    //console.log(data);
+    //console.log(data['names']);
     //metadata = array object
     //metadata is an array object that holds subjects information (id,gender,age,etc.)
     //use metadata to read the demographic info
     var metadata = data['metadata'];
-    console.log(metadata);
+    //console.log(metadata);
     //filter the metadata in order to retrieve a single id in the 153 items in the metadata array 
 
     // create an empty array to add 'names' to 
@@ -40,15 +40,28 @@ d3.json("samples.json").then(function(data) {
 
 function buildTable(id){
 d3.json("samples.json").then(function(data){
-  console.log(data['metadata']);
+  //console.log(data['metadata']);
   var metadata = data['metadata']
-  metadata.filter(sampleObj => sampleObj.id == id);
-  console.log('metadata')
+  var filteredData = metadata.filter(sampleObj => sampleObj.id == id);
+  console.log(filteredData);
+  var demo = document.getElementById('sample-metadata');
+  demo.innerHTML =  "<h6>" + "Age:" + filteredData[0]['age'] + "\n" + "Gender:" + filteredData[0]['gender'] 
+  + "\n" + "BB Type:" + filteredData[0]['bbtype'] + "\n" + "Ethnicity:" + filteredData[0]['ethnicity'] + 
+  "\n" + "Location:" + filteredData[0]['location'] +"\n" + "WFREQ:"+ filteredData[0]['wfreq']+"</h6>";
+  // demo.innerHTML =  "<pre>" + filte
+  // demo.innerHTML =  "<p>" + filteredData[0]['age'] + "\n" + filteredData[0]['gender'] + "\n" + filteredData[0]['bbtype'] + "\n" + filteredData[0]['ethnicity'] + "\n" + filteredData[0]['location'] +"\n" + filteredData[0]['wfreq']+"</p>";
+  // demo.innerHTML =  "<pre>" + filteredData[0]['bbtype'] + "\n" + filteredData[0]['ethnicity'] + "</pre>";
+  // demo.innerHTML += filteredData[0]["age"]
+  // demo.innerHTML += filteredData[0]['bbtype']
+  // demo.innerHTML += filteredData[0]['ethnicity']
+  // demo.innerHTML += filteredData[0]['gender']
+  // demo.innerHTML += filteredData[0]['id']
+  // demo.innerHTML += filteredData[0]['location']
+  // demo.innerHTML += filteredData[0]['wfreq']
 })
 }//end of buildTable Function
 
-buildTable("941")
-
 function optionChanged(id){
-  metadata(id);
+  buildTable(id);
 }
+
